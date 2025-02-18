30d9cd2510e7e6bef4e92bccf2a36a09b19291c0
Aman Khan <amankhancompq2002@gmail.com>
2025-02-19 01:53:57 +0530

    add navbar with responsive view

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,12 @@
 import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
