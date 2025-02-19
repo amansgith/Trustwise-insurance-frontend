282bce9ea8a280aabaa236a291232ae31e588e0b
ShaberManzoor <kshaber313@gmail.com>
2025-02-19 14:54:09 +0530

    about and hero section

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import { ArrowRight } from 'lucide-react'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="bg-[#f7f7f7] py-16">
+      <div className='max-w-screen-2xl mx-auto flex gap-10'>
+        <div className="flex flex-col justify-between">
+            <div className="w-fit flex flex-col gap-4">
+                <h3 className='uppercase text-[46px] leading-[56px] font-bold'>About Us</h3>
+                <p className='h-0.5 w-9/12 bg-[#2a80b4]'></p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <p className="font-medium">Founded in 2006, Aaxel Insurance is an independent insurance broker serving throughout Ontario and Alberta. Aaxel Insurance provides innovative Personal and Commercial Insurance solutions to its client’s Home, Auto, Life and Businesses through our trusted insurance partners.</p>
+              <p className="font-medium">
+                Aaxel Insurance deliver best-in-class insurance solutions backed by excellent customer service. We have locations across Ontario and Alberta and proudly serving thousands of customers.
+              </p>
+              <p className="font-medium">
+                Aaxel Insurance provides innovative insurance solutions for all your insurance needs at very comparative rates.
+              </p>
+            </div>
+            <div className="">
+            <button className='py-3.5 px-10 bg-[#9ecb75] hover:bg-[#2a80b4] hover:text-white flex items-center'>
+              Learn More <ArrowRight size={16} />
+            </button>
+          </div>
+        </div>
+          <img src="/Emblem.jpg" alt="" width={867} height={400} className='h-[400px]' />
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Hero.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const buttons = ["Auto", "Home", "Business", "Life & Financial", "Travel"];
+
+const Hero = () => {
+  return (
+    <div className="bg-white w-full flex px-6 items-center">
+      <div className='flex flex-col gap-5'>
+          <h3 className=' uppercase text-[30px] font-bold leading-normal'>
+            Your Coverage, Your Way!
+          </h3>
+          <h3 className='text-[64px] font-extrabold leading-[64px]'>
+            Save Up To 30% on <br />
+            Auto, Home and Business Insurance
+          </h3>
+          <p className='text-2xl'>Your Trusted Insurance Partner. Customized Solutions for Your Insurance Needs!</p>
+          <h3 className='text-xl font-bold'>
+            + Popular Products
+          </h3>
+          <div className="flex gap-2 items-center text-[16px] font-bold">
+            {
+              buttons.map((button, index) => (
+                <button key={index} className='p-3.5 uppercase rounded bg-[#9ecb75] hover:bg-[#2a80b4] hover:text-white'>
+                  {button}
+                </button>
+              ))
+            }
+            <p className='text-[#2cc8b3]'>+ MORE</p>
+          </div>
+          <div className="">
+            <button className='py-3.5 px-10 uppercase text-white bg-[#2a80b4] hover:bg-[#9ecb75] hover:text-black text-xl font-bold'>
+              Get a Quote
+            </button>
+          </div>
+      </div>
+      <img src="/bgHero.webp" alt="" />
+    </div>
+  )
+}
+
+export default Hero
