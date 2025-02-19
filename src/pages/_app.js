d26096183323d0d6fd703664c1baa1973b1d29e5
ShaberManzoor <kshaber313@gmail.com>
2025-02-19 22:39:38 +0530

    font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,12 +5,12 @@ const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   display: "swap",
-  variable: "--font-montserrat", // Add a CSS variable
+  className: "--font-montserrat", // Add a CSS variable
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={montserrat.variable}>
+    <div className={montserrat.className}>
       <Component {...pageProps} />
     </div>
   );
